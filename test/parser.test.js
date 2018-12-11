119f65c229559ed532788c31b0acab9ad1069f55
indexzero <charlie.robbins@gmail.com>
2018-12-11 00:23:04 -0500

    [test] Assume the manually verified "snapshot"

--- test/parser.test.js
+++ test/parser.test.js
@@ -16,6 +16,29 @@ describe('Parser', function () {
     const parser = new Parser();
     parser.parse(single);
 
-    console.dir(parser.books);
+    assume(parser.books).is.an('array');
+    assume(parser.books.length).equals(1);
+    assume(parser.books).deep.equals([{
+      'book-number': 332,
+      author: 'Alumit, Noel',
+      title: 'Letters to Montgomery Clift',
+      publisher: 'MacAdam/Cage',
+      description: 'Softcover Advance Proof copy of author\'s first novel.',
+      price: 6,
+      'last-updated': new Date('2010-06-15T04:00:00.000Z'),
+      categories: [ 'ARCs/Uncorrected Proofs' ],
+      ISBN: '1931561028',
+      edition: 'Uncorrected Proof',
+      'jacket-condition': 'No Jacket',
+      binding: 'Original Wraps',
+      'publish-place': 'San Francisco',
+      'publish-year': 2002,
+      'book-condition': 'NF',
+      size: '8vo - over 7¾" - 9¾" tall',
+      'book-type': 'Advanced Reading Copy (ARC)',
+      status: 'For Sale',
+      cost: 1,
+      comments: 'SHELF 9'
+    }])
   });
 });
