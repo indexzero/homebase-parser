c3ddf52a195e57c82ef7dfa0d69357dd1b6a49a9
indexzero <charlie.robbins@gmail.com>
2018-12-10 23:57:22 -0500

    [api] Initial parsing completed.

---
+++ parser.js
@@ -0,0 +1,82 @@
+const { names, ignore, control } = require('./fields');
+const diagnostics = require('diagnostics');
+
+/**
+ * Descriptive debug directives
+ * @type {Object}
+ */
+const debug = {
+  read: diagnostics('homebase:read'),
+  book: diagnostics('homebase:book'),
+  line: diagnostics('homebase:line'),
+  start: diagnostics('homebase:start')
+};
+
+/**
+ * Simple regular expression to parse the line. e.g
+ *
+ * BOOS|
+ * BOOK|000334
+ * TRAN|A
+ * ANAM|Niven, Jennifer
+ * TNAM|The Ice Master: The Doomed 1913 Voyage of the Karluk
+ * PBLS|Hyperion
+ * DESC|Softcover Advance Uncorrected Proof of author's first book
+ * PRIC|6
+ * BIND|S
+ * EDTN|F
+ * UPDT|6/15/2010
+ * UPTM|12:09:01 PM
+ * BCAT|Non-Fiction: Exploration/True Adventure
+ * BCAT|ARCs/Uncorrected Proofs
+ *
+ * @type {RegExp}
+ */
+const lexer = /(\w{4})\|(.*)$/;
+
+/**
+ * Parser is responsible for mapping fields into objects using
+ * control fields as a guide for boundaries between objects.
+ * @type {Parser}
+ */
+module.exports = class Parser {
+  constructor() {
+    this.books = [];
+    this.current = null;
+    this.parseLine = this.parseLine.bind(this);
+  }
+
+  parse(text) {
+    text
+      .split('\n')
+      .forEach(this.parseLine);
+  }
+
+  parseLine(line) {
+    debug.read(line);
+
+    const match = lexer.exec(line);
+    if (!match) {
+      debug.read('Could not match: "%s"', line);
+      return;
+    }
+
+    const [, field, value] = match;
+    if (field === control.end) {
+      debug.book("Parsed %j", this.current);
+      this.books.push(this.current);
+    } else if (field === control.start) {
+      debug.start();
+      this.current = {};
+    } else if (!ignore[field]) {
+      const name = names[field];
+      if (!name) {
+        debug.line('No name for: %s in "%s"', field);
+        return;
+      }
+
+      debug.line(field, name, value);
+      this.current[name] = value;
+    }
+  }
+}
---
+++ test/fixtures/index.js
@@ -0,0 +1,35 @@
+
+const single = `
+BOOS|
+BOOK|000332
+TRAN|A
+ANAM|Alumit, Noel
+TNAM|Letters to Montgomery Clift
+PBLS|MacAdam/Cage
+DESC|Softcover Advance Proof copy of author's first novel.
+PRIC|6
+BIND|S
+EDTN|F
+UPDT|6/15/2010
+UPTM|12:45:35 PM
+BCAT|ARCs/Uncorrected Proofs
+ABLE|1
+ISBN|1931561028
+UBID|367
+EDNT|Uncorrected Proof
+JCKC|No Jacket
+BNDC|Original Wraps
+PBPL|San Francisco
+PBYR|2002
+BOOC|NF
+SIZE|8vo - over 7¾" - 9¾" tall
+BTYP|Advanced Reading Copy (ARC)
+STAT|For Sale
+PPRC|1
+PDSC|SHELF 9
+BOOE|
+`;
+
+module.exports = {
+  single
+};
